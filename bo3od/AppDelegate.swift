97d4f7318026de98c11841b0344c71b49c874b30
bo3od017 <bo3od017@gmail.com>
2017-05-30 22:44:39 +0800

    first commit

---
+++ bo3od/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  bo3od
+//
+//  Created by Shine on 2017/5/30.
+//  Copyright © 2017年 Shine. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ bo3od/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  bo3od
+//
+//  Created by Shine on 2017/5/30.
+//  Copyright © 2017年 Shine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
